74c43e31fd8bab7d5d9629aa965a8ed54d4dafd8
Sam Morozov <109150200+sammorozov@users.noreply.github.com>
2024-04-21 18:25:21 +0300

    Update abuse.py

--- abuse.py
+++ abuse.py
@@ -2,7 +2,6 @@ import subprocess
 import time
 from datetime import datetime, timedelta
 
-# Формирование дат для перебора
 # start_date = datetime(2000, 1, 1)
 # end_date = datetime(2024, 12, 31)
 # delta = timedelta(days=1)
@@ -16,11 +15,9 @@ while current_date <= end_date:
     result = subprocess.run(curl_command, shell=True, capture_output=True, text=True)
     print(f"Sent request for date: {submission}, Response: {result.stdout}")
 
-    # Логирование ответа в файл (добавьте нужный путь к файлу)
     with open('response_log.txt', 'a') as f:
         f.write(f"Sent request for date: {submission}, Response: {result.stdout}\n")
 
-    # Время сна между запросами (в секундах)
     time.sleep(1)
 
     current_date += delta
